64cd753caa562c85094df537f5e17737f1ea7403
labsfiap <labsfiap@PA501MICRO11.fiap.com.br>
2024-10-21 22:22:44 -0300

    Atualização de endereço

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'RUA PANTALEAO BRAS',
+    450,
+    '05372080',
+    'JARDIM ESTER',
+    'SAO PAULO',
+    'BRASIL',
+    'CASA'
+);
